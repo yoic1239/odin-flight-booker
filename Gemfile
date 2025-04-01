85c14a932dfde81757d74c672004a5d516942d2d
Oi Ching Yip <67258773+yoic1239@users.noreply.github.com>
2025-04-01 12:54:37 -0600

    chore: Add flight seed date

--- Gemfile
+++ Gemfile
@@ -48,10 +48,12 @@ end
 group :development do
   # Use console on exceptions pages [https://github.com/rails/web-console]
   gem "web-console"
+  gem "faker"
 end
 
 group :test do
   # Use system testing [https://guides.rubyonrails.org/testing.html#system-testing]
   gem "capybara"
   gem "selenium-webdriver"
+  gem "faker"
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -8,8 +8,21 @@
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
 
+require 'faker'
+
 airport_codes = [ "HKG", "YEG", "YYC", "YVR", "FUK", "HND", "NRT", "KIX" ]
 
 airport_codes.each do |airport_code|
   Airport.find_or_create_by(airport_code: airport_code)
+end
+
+20.times do
+  departure, arrival = Airport.all.sample(2)
+
+  Flight.find_or_create_by(
+    departure_airport: departure,
+    arrival_airport: arrival,
+    start_datetime: Faker::Time.forward(days: 90),
+    duration_mins: rand(90..720) # Random between 30 minutes and 12 hours (720 minutes)
+    )
 end
