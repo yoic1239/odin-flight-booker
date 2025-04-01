49807241d067b617aa2c3c48abd5179f321ef3a0
Oi Ching Yip <67258773+yoic1239@users.noreply.github.com>
2025-03-31 21:54:50 -0600

    chore: Add airport seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,7 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+
+[ "HKG", "YEG", "YYC", "YVR", "FUK", "HND", "NRT", "KIX" ].each do |airport_code|
+  Airport.create(airport_code: airport_code)
+end
