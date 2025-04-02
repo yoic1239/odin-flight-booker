c8a2aa7e4b7b1af5dbafd53fa595c9db9f100c64
Oi Ching Yip <67258773+yoic1239@users.noreply.github.com>
2025-04-02 12:17:45 -0600

    feat: Add search flights form

--- app/controllers/flights_controller.rb
+++ app/controllers/flights_controller.rb
@@ -1,4 +1,6 @@
 class FlightsController < ApplicationController
   def index
+    @airport_options = Airport.pluck(:airport_code).sort
+    @flight_date_options = Flight.pluck(:start_datetime).map { |datetime| [ datetime.strftime("%Y %b %d"), datetime.to_date ] }.uniq.sort
   end
 end
