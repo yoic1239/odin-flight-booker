f6a30a8c5d6dd1252e01fa0a6fa090b3a13c2414
Oi Ching Yip <67258773+yoic1239@users.noreply.github.com>
2025-04-03 16:05:25 -0600

    feat: Add booking_id column in Passengers

---
+++ db/migrate/20250403220153_add_booking_ref_to_passengers.rb
@@ -0,0 +1,5 @@
+class AddBookingRefToPassengers < ActiveRecord::Migration[7.2]
+  def change
+    add_reference :passengers, :booking, null: false, foreign_key: true
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.2].define(version: 2025_04_03_211241) do
+ActiveRecord::Schema[7.2].define(version: 2025_04_03_220153) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -18,6 +18,13 @@ ActiveRecord::Schema[7.2].define(version: 2025_04_03_211241) do
     t.string "airport_code"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "bookings", force: :cascade do |t|
+    t.bigint "flight_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["flight_id"], name: "index_bookings_on_flight_id"
   end
 
   create_table "flights", force: :cascade do |t|
@@ -36,8 +43,12 @@ ActiveRecord::Schema[7.2].define(version: 2025_04_03_211241) do
     t.string "email"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.bigint "booking_id", null: false
+    t.index ["booking_id"], name: "index_passengers_on_booking_id"
   end
 
+  add_foreign_key "bookings", "flights"
   add_foreign_key "flights", "airports", column: "arrival_airport_id"
   add_foreign_key "flights", "airports", column: "departure_airport_id"
+  add_foreign_key "passengers", "bookings"
 end
