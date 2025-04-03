9ed6de0e2308ecc7e49ba7684c26d6328d22d7e4
Oi Ching Yip <67258773+yoic1239@users.noreply.github.com>
2025-04-02 21:35:30 -0600

    feat: Search flights with user selected criteria

--- app/controllers/flights_controller.rb
+++ app/controllers/flights_controller.rb
@@ -2,5 +2,27 @@ class FlightsController < ApplicationController
   def index
     @airport_options = Airport.pluck(:airport_code).sort
     @flight_date_options = Flight.pluck(:start_datetime).map { |datetime| [ datetime.strftime("%Y %b %d"), datetime.to_date ] }.uniq.sort
+
+    @results = search_flights if params[:num_tickets]
+  end
+
+  def search_flights
+    results = Flight.all
+
+    if params[:departure_code].present?
+      departure_airport = Airport.find_by(airport_code: params[:departure_code])
+      results = results.where(departure_airport: departure_airport)
+    end
+
+    if params[:arrival_code].present?
+      arrival_airport = Airport.find_by(airport_code: params[:arrival_code])
+      results = results.where(arrival_airport: arrival_airport)
+    end
+
+    if params[:date].present?
+      results = results.where("DATE(start_datetime) = ?", params[:date])
+    end
+
+    results
   end
 end
