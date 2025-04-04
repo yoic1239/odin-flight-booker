f9ed5eb67916b6e00acab4e46fb1170e20393e0b
Oi Ching Yip <67258773+yoic1239@users.noreply.github.com>
2025-04-04 13:20:40 -0600

    fix: Build new passenger object in controllers

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -3,6 +3,7 @@ class BookingsController < ApplicationController
     @flight = Flight.find(params[:flight])
     @num_tickets = params[:num_tickets].to_i
     @booking = Booking.new
+    @num_tickets.times { @booking.passengers.build }
   end
 
   def create
