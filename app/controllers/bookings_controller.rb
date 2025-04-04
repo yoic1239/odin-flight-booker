cb3b2640b95ea2f1507c4d1bf12a56cfbdc9b7de
Oi Ching Yip <67258773+yoic1239@users.noreply.github.com>
2025-04-04 14:53:16 -0600

    feat: Implement Booking create action

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -7,5 +7,20 @@ class BookingsController < ApplicationController
   end
 
   def create
+    @flight = Flight.find(params[:booking][:flight_id])
+
+    @booking = @flight.bookings.build(booking_params)
+
+    if @booking.save
+      redirect_to @booking
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def booking_params
+    params.require(:booking).permit(:flight_id, passengers_attributes: [ :name, :email ])
   end
 end
