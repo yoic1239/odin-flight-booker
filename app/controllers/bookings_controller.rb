95ce2d43bfd4c9520c510006a904fbf4252a8ac7
Oi Ching Yip <67258773+yoic1239@users.noreply.github.com>
2025-04-03 18:25:09 -0600

    feat: Show flight information in new booking form

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,5 +1,8 @@
 class BookingsController < ApplicationController
   def new
+    @flight = Flight.find(params[:flight])
+    @num_tickets = params[:num_tickets]
+    @booking = Booking.new
   end
 
   def create
